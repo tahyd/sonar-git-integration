34947750f5682bfcfeb6719e69128b09919fa6c4
tahyd <ta.hyd.003@gmail.com>
2025-02-18 19:37:18 +0530

    test removed

--- src/test/java/com/devops/sonar/SonarqubeJavaApplicationTests.java
+++ src/test/java/com/devops/sonar/SonarqubeJavaApplicationTests.java
@@ -3,10 +3,10 @@ package com.devops.sonar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SonarqubeJavaApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
